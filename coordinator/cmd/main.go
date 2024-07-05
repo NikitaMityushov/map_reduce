43c99ba972eff89590c67185b3a4aa24d54840aa
NikitaMityushov <nikitamityushov@gmail.com>
2024-07-05 15:26:31 +0300

    coordinator #2 added logger initialization

--- coordinator/cmd/main.go
+++ coordinator/cmd/main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/NikitaMityushov/map_reduce/coordinator/internal/config"
 )
 
 func main() {
-	fmt.Println("Coordinator is started")
-
 	// 1) config init
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
 	// 2) logger init
+	log := setupLogger(cfg.Env)
+
+	log.Info("Coordinator is started", slog.Any("cfg", cfg))
+
 	// 3) init app
+
 	// 4) start grpc server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "local":
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case "dev":
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case "prod":
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
